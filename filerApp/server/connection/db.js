b69f43709ae6b3d25864a62a92347875e2cc9362
hunktuck <ddikshakk@gmail.com>
2024-01-19 13:10:07 +0530

    all set

--- filerApp/server/connection/db.js
+++ filerApp/server/connection/db.js
@@ -1,11 +1,22 @@
 const Pool = require("pg").Pool;
 
-const pool = new Pool({
-    "user": "root",
-    "password" : "root",
-    "host" : "localhost",
-    "port" : 5432,
-    "database" : "test_db"
-})
+const getClient= async ()=> {
 
-module.exports= pool
+    try{
+        const client = new Pool({
+            "user": "root",
+            "password" : "root",
+            "host" : "localhost",
+            "port" : 5432,
+            "database" : "test_db"
+        });
+        await client.connect()
+        console.log('Postgtes connected successfully!');
+        return client
+    }catch(err){
+        console.log(err);
+    }
+
+}
+
+module.exports= getClient
--- filerApp/server/index.js
+++ filerApp/server/index.js
@@ -1,5 +1,5 @@
 const express= require('express')
-const pool= require('./connection/db')
+const getClient= require('./connection/db')
 const cors= require('cors')
 
 const dotenv= require('dotenv')
@@ -8,18 +8,7 @@ dotenv.config()
 const app = express();
 app.use(express.json())
 
-pool.connect()
-  .then(() => {
-    console.log('Connected to PostgreSQL database successfully!');
-    // Do any database operations here if needed
-  })
-  .catch((error) => {
-    console.error('Error connecting to PostgreSQL:', error.message);
-  })
-  .finally(() => {
-    // Close the connection after checking
-    pool.end();
-  });
+getClient()
 
 app.listen(process.env.PORT, ()=> {
     console.log(`Server listening on port ${process.env.PORT}`);
