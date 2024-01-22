333d286f37696ffd74431798f896ef112dbf385f
hunktuck <ddikshakk@gmail.com>
2024-01-22 12:43:50 +0530

    redis problem solved

--- realtime-design/index.js
+++ realtime-design/index.js
@@ -10,38 +10,38 @@ const server= http.createServer(app)
 const io= new Server(server)
 const redis = require("redis");
 const client = redis.createClient();
+client.on('connect', () => {
+    console.log('Connected to Redis');
+});
 client.on('error', err => console.log('Redis Client Error', err));
 
-client.connect()
 
-const getChat = (socket)=> {
-    console.log('dfdfdfdfdfd');
-   client.lRange("messages", "0" , "-1" , (err,data) => {
-        console.log('hhhhhhh');
-        data.map(val => {
-            const userNameMessage= val.split(":");
-            const redisUsername= userNameMessage[0];
-            const redisMesage= userNameMessage[1];
-            console.log(redisUsername,redisMesage);
-            
-            socket.emit("message" ,{
+function sendMessage(socket) {
+    client.lrange("messages", "0", "-1", (err, data) => {
+        data.map(x => {
+            const usernameMessage = x.split(":");
+            const redisUsername = usernameMessage[0];
+            const redisMessage = usernameMessage[1];
+
+            socket.emit("message", {
                 from: redisUsername,
-                message: redisMesage
-            })
-        })
-    } )
+                message: redisMessage
+            });
+        });
+    });
 }
-
 io.on("connection", socket => {
-    getChat(socket)
-    socket.on("message", async({message, from})=> {
-        client.rPush("messages", `${from}:${message}`) // key= message, value= "from:message"
-        io.emit("message", {message,from}) // here we have written io instead of socket.emit because io is responsible to send the message to all the users connected to our server. If we would have used just the socket then it would send message to the user who have made the message
-    } )
-})
+    sendMessage(socket);
+
+    socket.on("message", ({ message, from }) => {
+        client.rPush("messages", `${from}:${message}`);
+
+        io.emit("message", { from, message });
+    });
+});
 
 app.get('/chat', async(req,res)=> {
-    getChat()
+    
     const username= req.query.username
     io.emit("joined", username)
     res.render("chat",{username})
