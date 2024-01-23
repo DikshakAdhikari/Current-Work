fdac765b145aa17494b485f1f3fe8ec02e9a0e82
hunktuck <ddikshakk@gmail.com>
2024-01-24 00:47:40 +0530

    static router added

--- rate-limiter/index.js
+++ rate-limiter/index.js
@@ -1,11 +1,18 @@
 const express=  require("express")
 const app= express()
 const path= require("path")
+const staticRoute= require("./routes/staticRouter")
 app.use(express.json())
 
+app.set('view engine', "ejs")
+app.set("views", path.resolve('./views')) //app.set("views", path.join(__dirname, 'views'));
+
+app.use('/', staticRoute )
 
 
-
+// app.get('/', (req,res)=> {
+//     res.render("home")
+// })
 
 
 
