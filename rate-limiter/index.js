67526e93a56ce9004ff4a79b8524d1aec3fd3c59
hunktuck <ddikshakk@gmail.com>
2024-01-24 13:00:18 +0530

    ejs logic implemented

--- rate-limiter/index.js
+++ rate-limiter/index.js
@@ -2,13 +2,14 @@ const express=  require("express")
 const app= express()
 const path= require("path")
 const staticRoute= require("./routes/staticRouter")
+const userRouter= require("./routes/userRouter")
 app.use(express.json())
 
 app.set('view engine', "ejs")
 app.set("views", path.resolve('./views')) //app.set("views", path.join(__dirname, 'views'));
 
 app.use('/', staticRoute )
-
+app.use('/user', userRouter)
 
 // app.get('/', (req,res)=> {
 //     res.render("home")
--- rate-limiter/routes/staticRouter.js
+++ rate-limiter/routes/staticRouter.js
@@ -1,9 +1,21 @@
 const express= require("express")
 const router= express.Router()
 
+let url1Items=[]
 router.get('/', (req,res)=> {
-    res.render('home')
-})
+    res.render('home',{
+        url1data: url1Items
+    })
+});
 
+router.get('/url1', async(req,res)=> {
+    try{
+        let url1Output ="Dikshak is a master"  //newItem= req.body.name
+         url1Items.push(url1Output)
+        res.redirect("/")
+    }catch(err){
+        res.status(403).json({message:err})
+    }
+})
 
 module.exports= router
