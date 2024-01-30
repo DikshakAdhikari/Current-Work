b8a9db655ea6ac677ef0d054fea29b33fb850afb
hunktuck <ddikshakk@gmail.com>
2024-01-30 23:33:09 +0530

    all set

--- multiple-private-realtime/server/index.js
+++ multiple-private-realtime/server/index.js
@@ -1,11 +1,21 @@
 const express= require('express');
 const connect = require('./connection/connect');
+const createServer= require("http").createServer
+const {Server}= require('socket.io')
 const port= 4000
 
 const app= express();
+const server= createServer(app)
+
+const io= new Server(server)
+
 
 connect()
 app.use(express.json())
+
+app.get('/', (req, res) => {
+    res.send('<h1>Hello world</h1>');
+  });
 
 
 
@@ -14,4 +24,6 @@ app.listen(port , ()=> {
 })
 
 
- 
+ io.on('connection' , (socket)=> {
+    console.log('a user connected!');
+ })
