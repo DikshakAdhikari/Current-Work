508bdc1ce05089aadf97cb8f7479d46eea642ec6
hunktuck <ddikshakk@gmail.com>
2024-02-11 23:57:29 +0530

    socket server initialized

--- chatApp/server/index.js
+++ chatApp/server/index.js
@@ -1,7 +1,12 @@
 const express = require('express')
+const {createServer} = require("http");
+const { Server } = require('socket.io');
 const PORT= 3000;
 const app= express()
 app.use(express())
+const server= createServer(app)
+
+const io= new Server(server)
 
 app.get('/', (req,res)=> {
     res.send('All set!')
