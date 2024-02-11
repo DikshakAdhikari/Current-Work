732c63b876de4e5fc1489d9cb04203405be8dc78
hunktuck <ddikshakk@gmail.com>
2024-02-11 12:34:40 +0530

    new

--- multiple-private-realtime/server/index.js
+++ multiple-private-realtime/server/index.js
@@ -12,16 +12,14 @@ const io = new Server(server ,{
   }
 });
 
-// app.get('/', (req, res) => {
-//   res.sendFile(join(__dirname, 'index.html'));
-// });
 
 io.on("connect", (socket)=> {
   // console.log(socket.id);
   socket.emit("message", socket.id)
   socket.on("chat", ({text, room})=> {
     // console.log(text);
-    io.to(room).emit("user:chat", {text,room})
+    io.to(room).emit("user:chat", {text,room}) 
+
   } )
    socket.on("join", (room)=> {
      socket.join(room)
