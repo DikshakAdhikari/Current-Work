5478a26277a49e639d89c7ee70500fc3ff7e210e
hunktuck <ddikshakk@gmail.com>
2024-02-11 23:52:16 +0530

    all set

--- chatApp/server/index.js
+++ chatApp/server/index.js
@@ -3,6 +3,10 @@ const PORT= 3000;
 const app= express()
 app.use(express())
 
+app.get('/', (req,res)=> {
+    res.send('All set!')
+})
+
 app.listen(PORT , ()=> {
     console.log('Server listening on port '+ PORT);
 });
