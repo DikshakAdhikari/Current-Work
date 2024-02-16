bdc7db2e858956d33b941c4a0ae9be7ac56c732c
hunktuck <ddikshakk@gmail.com>
2024-02-16 23:15:26 +0530

    chat client implemented

--- chatApp/client/src/app/chat/page.js
+++ chatApp/client/src/app/chat/page.js
@@ -6,7 +6,7 @@ import { BASE_URL } from '../secret'
 
 const page = () => {
   const socket= useRef()
-
+  const [contacts, setContacts]= useState([])
   useEffect(()=> {
      socket.current = io(`${BASE_URL}`,{
       withCredentials:true
@@ -18,17 +18,52 @@ const page = () => {
     const {id}= JSON.parse(localStorage.getItem('token'))
     console.log(id);
     socket.current.emit("add-user", id )
-  },[])
+  },[]);
+
+  useEffect(()=> {
+    const fun = async ()=> {
+      try{
+        const {id}= JSON.parse(localStorage.getItem('token'))
+        const res= await fetch(`${BASE_URL}/user/all/${id}`,{
+          method:"GET",
+          headers:{
+            'Content-Type': "application/json"
+          }
+        });
+        if(!res.ok){
+          throw new Error("Network Error!");
+        }
+        const data= await res.json();
+        setContacts(data)
+      }catch(err){
+        console.log(err);
+      }
+    }
+    fun()
+  })
   
     
   return (
     <div className=' w-[100vw]'>
       <ChatNav />
       <div className=' h-[93vh] w-[100vw] flex'>
-        <div className=' border-r-4 border-gray-300 w-[20vw] '>
+        <div className=' border-r-4 border-gray-300 p-3 w-[20vw] '>
+          {
+            contacts.length === 0 ?
+            <div>No contacts available</div> :
+            <div>
+              {
+                contacts.map((val,index)=> (
+                  <div key={val._id}> 
+                      <div> {val.username} </div>
+                  </div>
+                ))
+              }
+            </div>
+           }
         </div>
-        <div className=' border-r-4 bg-black w-[100vw] border-gray-300  '>
-            fd
+        <div className=' border-r-4 bg-black w-[100vw] text-white p-4 border-gray-300  '>
+          fdf
         </div>
       </div>
     </div>
