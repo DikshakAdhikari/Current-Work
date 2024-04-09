d76d00f4c5cbfe4eabd9afcf59cb902811edd1d0
hunktuck <ddikshakk@gmail.com>
2024-04-10 00:06:07 +0530

    final dep

--- dribble/server/routes/userRouter.js
+++ dribble/server/routes/userRouter.js
@@ -1,20 +1,8 @@
 import express from 'express'
-// import { insertUserController } from '../controllers/userController.js'
-import userModel from '../models/userModel.js'
+import { insertUserController } from '../controllers/userController.js'
 const userRouter= express.Router()
 
-userRouter.post('/', async (req,res)=> {
-    try{
-        const {name, username, email, password}= req.body;
-        const insertt= await userModel.create({
-            name, username, email, password
-        })
-        await insertt.save()
-        const user= await userModel.findOne({email})
-        res.status(200).json({userId:user._id})
-    }catch(err){
-    }
-})
+userRouter.post('/', insertUserController)
 
 
 export default userRouter
