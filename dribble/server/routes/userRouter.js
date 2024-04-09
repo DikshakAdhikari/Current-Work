06a9290c7f75e92349dcb45b4ba1b5724ee08fbb
hunktuck <ddikshakk@gmail.com>
2024-04-09 22:50:24 +0530

    controller removed

--- dribble/server/routes/userRouter.js
+++ dribble/server/routes/userRouter.js
@@ -1,8 +1,19 @@
 import express from 'express'
-import { insertUserController } from '../controllers/userController.js'
+// import { insertUserController } from '../controllers/userController.js'
 const userRouter= express.Router()
 
-userRouter.post('/', insertUserController)
+userRouter.post('/', async (req,res)=> {
+    try{
+        const {name, username, email, password}= req.body;
+        const insertt= await userModel.create({
+            name, username, email, password
+        })
+        await insertt.save()
+        const user= await userModel.findOne({email})
+        res.status(200).json({userId:user._id})
+    }catch(err){
+    }
+})
 
 
 export default userRouter
