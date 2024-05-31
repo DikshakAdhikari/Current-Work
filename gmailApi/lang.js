24b6e4291f2277443ff87437700cda13c86b0355
hunktuck <ddikshakk@gmail.com>
2024-05-31 23:26:52 +0530

    lang done

---
+++ gmailApi/lang.js
@@ -0,0 +1,27 @@
+
+const {ChatPromptTemplate} = require("@langchain/core/prompts");
+const { ChatGroq } = require("@langchain/groq");
+
+const model = new ChatGroq({
+  apiKey: '',
+});
+const prompt = ChatPromptTemplate.fromMessages([
+  ["system", "You are a helpful assistant"],
+  ["human", "{input}"],
+]);
+
+const chain = prompt.pipe(model);
+const fun = async ()=> {
+    const response =  await chain.invoke({
+        input: "Hello",
+      });
+      console.log("response", response);
+}
+
+fun()
+
+/**
+response AIMessage {
+  content: "Hello! I'm happy to assist you in any way I can. Is there something specific you need help with or a question you have?",
+}
+ */
