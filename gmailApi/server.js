64363d38b1948fb3b7ab97cbe0d3f4f03a960ac6
hunktuck <ddikshakk@gmail.com>
2024-06-16 22:58:17 +0530

    new route made

--- gmailApi/server.js
+++ gmailApi/server.js
@@ -11,7 +11,13 @@ app.use(express.json())
 
 app.use('/user', userRouter)
 
-
+app.length(':id', async(req,res)=> {
+    try{
+        
+    }catch(err){
+        res.json(err);
+    }
+})
 app.listen(process.env.PORT || 5000, ()=> {
     console.log(`Server listening on port ${process.env.PORT}`);
 })
