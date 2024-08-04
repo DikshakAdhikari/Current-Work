d507b04b1bf81e548590dc27b93a13b5d789f95e
hunktuck <ddikshakk@gmail.com>
2024-08-04 19:12:07 +0530

    done

--- stripe-webhook/index.js
+++ stripe-webhook/index.js
@@ -1,9 +1,15 @@
 const express= require("express")
+const dotenv= require('dotenv')
+dotenv.config()
+const stripe= require('stripe')(process.env.STRIPE_PASSWORD)
 
 const app=express()
 
 app.use(express.json());
 
+app.get('/',(req,res)=> {
+    res.send("Hello maannnnnaa")
+})
 
 
 app.listen(5000, ()=> {
