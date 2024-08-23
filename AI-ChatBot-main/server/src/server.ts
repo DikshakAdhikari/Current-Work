6763c2a91065214b506ff5fe52abe39eddddb1c6
hunktuck <ddikshakk@gmail.com>
2024-08-23 10:52:06 +0530

    new dep

--- AI-ChatBot-main/server/src/server.ts
+++ AI-ChatBot-main/server/src/server.ts
@@ -9,10 +9,7 @@ const app = express();
 
 // Middleware 
 app.use(morgan("dev"))
-app.use(cors({
-    origin: ["http://localhost:3000"],
-    credentials: true
-}));
+app.use(cors({ origin: "*", methods: ["GET", "POST", "DELETE", "PUT"] }));
 app.use(express.json())
 
 // Health route
